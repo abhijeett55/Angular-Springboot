8af949b367ab668ab8d26b357d16b565fb80395a
abhijeett55 <100180572+abhijeett55@users.noreply.github.com>
2026-05-03 18:28:20 +0530

    push some changes

---
+++ src/app/Components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'header',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+
+export class HeaderComponent {
+  locationText: string = '';
+}
+
